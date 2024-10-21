282ed2385eeaaa0d800ce4f66843da21128e4178
hitesh-mindfire <mfsi.hiteshs@gmail.com>
2024-10-21 12:28:59 +0530

    query updated with latest result

--- src/services/Query.ts
+++ src/services/Query.ts
@@ -3,7 +3,7 @@ import { gql } from "@apollo/client";
 export const GET_REPOSITORIES = gql`
   query ($login: String!) {
     user(login: $login) {
-      repositories(first: 10) {
+      repositories(first: 10, orderBy: { field: CREATED_AT, direction: DESC }) {
         nodes {
           name
           description
